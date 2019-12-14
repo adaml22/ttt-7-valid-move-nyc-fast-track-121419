a737fedbf693bbd763274a97fdbd7aba20bfa026
Adam Longman <boardhead89@gmail.com>
2019-12-14 20:48:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 # code your #valid_move? method here
 
 def valid_move?(board, index)
@@ -11,7 +13,7 @@ def valid_move?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 
 def position_taken?(board, index)
   if board[index] == "O"
